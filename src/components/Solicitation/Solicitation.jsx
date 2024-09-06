70ac2fbd8ba02ebeb7ca10b36604a9260e49a2ba
marianovungel <vungem88@gmail.com>
2024-09-06 12:55:10 -0300

    rejeitar

--- src/components/Solicitation/Solicitation.jsx
+++ src/components/Solicitation/Solicitation.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Solicitation.css'
 import { BsThreeDots } from "react-icons/bs";
 import { FaRegCircleXmark } from "react-icons/fa6";
 import { FaRegCircleCheck } from "react-icons/fa6";
+import { toast } from 'react-toastify';
+import { api_base_url } from '../../Helper';
 
 export default function Solicitation({ data }) {
+    const [datas, setDatas] = useState([])
+    
+    const RegectSolicitation = async ()=>{
+        await fetch(api_base_url + "/reject", {
+          mode:"cors",
+          method: "POST",
+          headers:{
+            "Content-Type":"application/json",
+          },
+          body: JSON.stringify({
+            notifyId: data?._id,
+          }),
+        })
+        .then((res)=> res.json())
+          .then((data)=>{
+              setDatas(data.notification)
+              console.log(data.notification)
+              toast.success("Solicitação Rejeitada com sucesso!")
+          })
+        }
   return (
     <div id='rejectedContainer' className='w-full flex flex-row items-center justify-between bg-blend-lighten gap-3 py-3 hover:bg-cyan-50'>
         <div className='flex flex-row items-center justify-start gap-3 px-3'>
@@ -17,7 +39,7 @@ export default function Solicitation({ data }) {
         <div className='px-3 h-full flex flex-col items-end justify-center gap-3'>
             <BsThreeDots size={24} color='gray' />
             <div className='flex flex-row items-center justify-end gap-3'>
-                <FaRegCircleXmark size={35} />
+                <FaRegCircleXmark size={35} onClick={RegectSolicitation} />
                 <FaRegCircleCheck size={35} className='text-sky-700' />
             </div>
         </div>
